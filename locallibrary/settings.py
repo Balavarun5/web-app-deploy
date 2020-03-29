45ffd8f58c67f148a0cb4ec8f045c21294f3c403
Balavarun P <balavarunp@Balavaruns-MacBook-Pro.local>
2020-03-29 23:12:21 +0530

    New app settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1o7%t%hq%70!+fb#r!(xzpa%i=xm2#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+
 
 ALLOWED_HOSTS = ['calm-temple-23148.herokuapp.com', '127.0.0.1']
 
